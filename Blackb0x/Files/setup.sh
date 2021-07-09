13dfb71ed06db9c67021be32cec759cf25600f24
JTV <54797110+joshblah555@users.noreply.github.com>
2021-07-09 13:36:00 -0600

    Adding a /wip/ PlexConnect

--- Blackb0x/Files/setup.sh
+++ Blackb0x/Files/setup.sh
@@ -11,19 +11,6 @@ echo "Starting post-install" > /var/mobile/Media/blackb0x.log
 #Update firmware info for Cydia
 /bin/sh /usr/libexec/cydia/firmware.sh
 
-echo "Adding replacement default repository" >> /var/mobile/Media/blackb0x.log
-
-/bin/rm -rf /etc/apt/sources.list.d/awkward.list
-/bin/rm -rf /etc/apt/sources.list.d/awkwardtv.list
-
-if [ ! -f /etc/apt/sources.list.d/joshtv.list ]; then
-    echo "Installing JoshTV Repo" >> /var/mobile/Media/blackb0x.log
-    mv /joshtv.list /etc/apt/sources.list.d/
-    /usr/bin/apt-key add /pubkey.key
-fi
-
-
-
 #Update apt
 echo "Updating apt" >> /var/mobile/Media/blackb0x.log
 apt-get update || echo "Update failed" >> /var/mobile/Media/blackb0x.log 
@@ -65,25 +52,47 @@ fi
 
 #Debs fixed by JoshTV
 echo "Installing debs" >> /var/mobile/Media/blackb0x.log
+mv /ldid_1-1.2.1_iphoneos-arm.deb /private/var/cache/apt/archives/ldid_1-1.2.1_iphoneos-arm.deb
 mv /rtadvd_307.0.1-2_iphoneos-arm-fixed.deb /private/var/cache/apt/archives/rtadvd_307.0.1-2_iphoneos-arm-fixed.deb
 mv /sqlite3-dylib_3.5.9-1_iphoneos-arm-fixed.deb /private/var/cache/apt/archives/sqlite3-dylib_3.5.9-1_iphoneos-arm-fixed.deb
 mv /sqlite3-lib_3.5.9-2_iphoneos-arm-fixed.deb /private/var/cache/apt/archives/sqlite3-lib_3.5.9-2_iphoneos-arm-fixed.deb
 mv /com.saurik.patcyh_1.2.0_iphoneos-arm-fixed.deb /private/var/cache/apt/archives/com.saurik.patcyh_1.2.0_iphoneos-arm-fixed.deb
 mv /uikittools_1.1.12_iphoneos-arm-fixed.deb /private/var/cache/apt/archives/uikittools_1.1.12_iphoneos-arm-fixed.deb
+mv /beigelist_2.2.6-30_iphoneos-arm.deb /private/var/cache/apt/archives/beigelist_2.2.6-30_iphoneos-arm.deb
+mv /com.nito.updatebegone_0.2-1_iphoneos-arm.deb /private/var/cache/apt/archives/com.nito.updatebegone_0.2-1_iphoneos-arm.deb
 
+
+/usr/bin/dpkg -i /private/var/cache/apt/archives/ldid_1-1.2.1_iphoneos-arm.deb
 /usr/bin/dpkg -i /private/var/cache/apt/archives/rtadvd_307.0.1-2_iphoneos-arm-fixed.deb
 /usr/bin/dpkg -i /private/var/cache/apt/archives/sqlite3-dylib_3.5.9-1_iphoneos-arm-fixed.deb
 /usr/bin/dpkg -i /private/var/cache/apt/archives/sqlite3-lib_3.5.9-2_iphoneos-arm-fixed.deb
 /usr/bin/dpkg -i /private/var/cache/apt/archives/com.saurik.patcyh_1.2.0_iphoneos-arm-fixed.deb
 /usr/bin/dpkg -i /private/var/cache/apt/archives/uikittools_1.1.12_iphoneos-arm-fixed.deb
 
-
 echo "Installing substrate" >> /var/mobile/Media/blackb0x.log
 apt-get install -y mobilesubstrate
+#apt-get install -f -y
+
+/usr/bin/dpkg -i /private/var/cache/apt/archives/beigelist_2.2.6-30_iphoneos-arm.deb
+/usr/bin/dpkg -i /private/var/cache/apt/archives/com.nito.updatebegone_0.2-1_iphoneos-arm.deb
 
 /usr/bin/apt-get upgrade -y || echo "Upgrade failed" >> /var/mobile/Media/blackb0x.log
 
-apt-get install -f -y
+echo "Adding replacement default repository" >> /var/mobile/Media/blackb0x.log
+
+if [ -f /etc/apt/sources.list.d/awkward.list ]; then
+echo "AwkwardTV detected. Replacing..." >> /var/mobile/Media/blackb0x.log
+/bin/rm -rf /etc/apt/sources.list.d/awkward.list
+mv /joshtv.list /etc/apt/sources.list.d/
+/usr/bin/apt-key add /pubkey.key
+else
+echo "AwkwardTV doesn't exist. Adding replacement..." >> /var/mobile/Media/blackb0x.log
+mv /joshtv.list /etc/apt/sources.list.d/
+/usr/bin/apt-key add /pubkey.key
+fi
+
+
+
 
 #Install Apps (nitoTV, Kodi)
 #nitoTV and Kodi icons 1080p (Credit: JoshTV)
@@ -92,10 +101,6 @@ if [ ! -d /Applications/AppleTV.app/Appliances/nitoTV.frappliance ]; then
 	echo "Installing nitoTV" >> /var/mobile/Media/blackb0x.log
 	apt-get -y install com.nito.nitoTV
 	/bin/mv /nito.png /private/var/stash/Applications/AppleTV.app/com.nito.frontrow.appliance.nitoTV\@1080.png
- 
-    if [ -d /Applications/Kodi.frappliance ]; then
-        killall -9 backboardd
-    fi
 
 fi
 
@@ -110,10 +115,21 @@ echo "Install AFC2" >> /var/mobile/Media/blackb0x.log
 apt-get -y install com.saurik.afc2d
 #apt-get install us.scw.afctwoadd
 
+# WIP - will test and finish later...
+#echo "Install Python" >> /var/mobile/Media/blackb0x.log
+#apt-get -y install python
+
+#echo "Setting up PlexConnect..." >> /var/mobile/Media/blackb0x.log
+#apt-get -y install net.joshtv.plexconnect 
+#cd /Applications/PlexConnect/support/aTV_jailbreak/
+#/bin/bash ./inst_atv3.sh
+
 
 #Finish installation
 echo "Finished installation" >> /var/mobile/Media/blackb0x.log
 echo "" > /private/var/mobile/.blackb0x_installed
 
 
+
+killall -9 backboardd
 
